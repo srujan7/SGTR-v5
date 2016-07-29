8cb7fc4870d882647854d9ec1adf542129d755ad
Srujan <srujan_barai@yahoo.com>
2016-07-29 18:20:18 +0530

    added GujaratiMapper for Gujarati typing

--- app/src/main/java/example/srujan/com/sgtrv5/FragmentEdit.java
+++ app/src/main/java/example/srujan/com/sgtrv5/FragmentEdit.java
@@ -1,24 +1,47 @@
 package example.srujan.com.sgtrv5;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
 /**
  * Created by Srujan on 09-05-2016.
  */
 public class FragmentEdit extends Fragment{
 
     final static String TAG = "FargmentEdit";
+    EditText mainEditText;
     View rootView;
 
     @Nullable//
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_edit,container,false);
-        return view;
+        rootView = inflater.inflate(R.layout.fragment_edit,container,false);
+
+        initViews();
+        setEditText();
+
+        return rootView;
+
+    }
+
+    private void initViews() {
+        mainEditText = (EditText) rootView.findViewById(R.id.mainEditText);
+
+    }
+
+    private void setEditText() {
+        //set font type
+        Typeface tf = Typeface.createFromAsset(getActivity().getAssets(),"shruti.ttf");
+        mainEditText.setTypeface(tf);
+
+        //set Gujarati mapper
+        mainEditText.addTextChangedListener(new GujaratiMapper(mainEditText, getContext()));
     }
 
 }
